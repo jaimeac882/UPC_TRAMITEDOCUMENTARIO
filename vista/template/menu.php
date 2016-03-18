3de762f0a8434d55e106e90a19a4eb92616ae59f
jaimeac882 <u21215428@upc.edu.pe>
2016-03-18 01:27:51 -0500

    Cambioi en la BD  por el href del modulo
    y agrendo lo del menu los items que deberian ir

--- vista/template/menu.php
+++ vista/template/menu.php
@@ -15,6 +15,9 @@
                                   <?php if( $_SESSION["cod_user"] == 'USU002'){  ?>
                                   <li   class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span><a href="../vista/Activar_Tramite.php">Activar Tramites</a>
                                   <?php } ?>
+                                  <li   class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span><a style="color: red" href="../vista/Atender_Tramite.php">Atender Tramite (Trabajador de Area)</a>
+                                     <li   class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span><a style="color: red" href="../vista/Generar_RTramite.php">Aprobar Respuesta de Trámite (Jefe del Area)</a>
+                             
                                   <?php if( $_SESSION["cod_user"] == 'USU004'){  ?>
                                   <li class="list-group-item"><span class="glyphicon glyphicon-edit text-primary"></span> <a href="../vista/Delegar_Tramite.php">Delegar Carga de Trabajo Expediente</a></li>
                                   <?php } ?>
@@ -80,8 +83,11 @@
                         </h4>
                     </div>
                     <div id="collapseFour" class="panel-collapse collapse in">
-                         <!--<ul class="list-group">
-                            <li class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span> <a>Blogs</a>
+                         <ul class="list-group">
+                           
+                               <li   class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span><a style="color: red" href="../vista/Generar_RTramite.php">Generar Reporte de Trámite (Jefe de Tramite Documentario)</a>
+                               
+<!--                             <li class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span> <a>Blogs</a>
                                 <ul class="list-group">
                                     <li class="list-group-item"><span class="glyphicon glyphicon-minus text-primary"></span> <a>Edit Blog</a></li>
                                     <li class="list-group-item"><span class="glyphicon glyphicon-minus text-success"></span> <a>Publish Blog</a></li>
@@ -91,9 +97,33 @@
                             <li class="list-group-item"><span class="glyphicon glyphicon-edit text-success"></span> <a>Contact us</a></li>
                             <li class="list-group-item"><span class="glyphicon glyphicon-share-alt text-info"></span> <a>Share</a></li>
                             <li class="list-group-item"><span class="glyphicon glyphicon-bell text-success"></span> <a>Users online</a><span class="badge">7</span></li>
-                       --></ul>
+                      -->
+                         </ul>
                     </div>
 
+                     <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive"><span class="glyphicon glyphicon-plus"></span> Administrado</a>
+                        </h4>
+                    </div>
+                    <div id="collapseFive" class="panel-collapse collapse in">
+                         <ul class="list-group">
+                           
+                               <li   class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span><a style="color: red" href="../vista/Generar_RTramite.php">Consultar Estado de Tramite(Administrado)</a>
+                                
+<!--                             <li class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span> <a>Blogs</a>
+                                <ul class="list-group">
+                                    <li class="list-group-item"><span class="glyphicon glyphicon-minus text-primary"></span> <a>Edit Blog</a></li>
+                                    <li class="list-group-item"><span class="glyphicon glyphicon-minus text-success"></span> <a>Publish Blog</a></li>
+                                    <li class="list-group-item"><span class="glyphicon glyphicon-minus text-warning"></span> <a>Delete Blog</a></li>
+                                </ul>
+                            </li>
+                            <li class="list-group-item"><span class="glyphicon glyphicon-edit text-success"></span> <a>Contact us</a></li>
+                            <li class="list-group-item"><span class="glyphicon glyphicon-share-alt text-info"></span> <a>Share</a></li>
+                            <li class="list-group-item"><span class="glyphicon glyphicon-bell text-success"></span> <a>Users online</a><span class="badge">7</span></li>
+                      -->
+                         </ul>
+                    </div>
 
                 </div>
             </div>
